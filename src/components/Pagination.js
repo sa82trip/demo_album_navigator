627fd3999c243eaa40ab22a9d4a5ecf1d1843ea6
Joonsung Gil <sa82trip@gmail.com>
2021-06-07 10:42:21 +0900

    perf(pagination.js): modified appearnce of pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,24 +1,48 @@
 import React from "react";
 
-const Pagination = ({ imagesPerPage, totalImages, paginate }) => {
+const Pagination = ({ imagesPerPage, totalImages, paginate, currentPage }) => {
   const pageNumbers = [];
   for (let i = 1; i <= Math.ceil(totalImages / imagesPerPage); i++) {
     pageNumbers.push(i);
   }
   return (
     <div className="pb-10 ml-5">
-      <ul className="flex flex-wrap">
-        {pageNumbers.map((number) => (
-          <li className="mx-1">
-            <span
-              onClick={() => paginate(number)}
-              className="font-semibold text-blue-500 cursor-pointer"
-            >
-              {number}
+      <div className="flex ">
+        {currentPage !== 1 ? (
+          <div className="text-2xl">
+            <span className="mx-1" onClick={() => paginate(pageNumbers[0])}>
+              &#8606;
             </span>
-          </li>
-        ))}
-      </ul>
+            <span className="mx-1" onClick={() => paginate(currentPage - 1)}>
+              &larr;
+            </span>
+          </div>
+        ) : (
+          <div>
+            <span className="mx-1">&#160;</span>
+            <span className="mx-1">&#160;</span>
+          </div>
+        )}
+        <span className="text-2xl underline">{currentPage}</span>
+        {currentPage === pageNumbers.length ? (
+          <div>
+            <span className="mx-1">&#160;</span>
+            <span className="mx-1">&#160;</span>
+          </div>
+        ) : (
+          <div className="text-2xl">
+            <span className="mx-1" onClick={() => paginate(currentPage + 1)}>
+              &rarr;
+            </span>
+            <span
+              className="mx-1"
+              onClick={() => paginate(pageNumbers[pageNumbers.length - 1])}
+            >
+              &#8608;
+            </span>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
